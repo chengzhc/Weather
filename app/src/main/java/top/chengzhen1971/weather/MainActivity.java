8195659f7c80c278a51369a1390b7315a262561d
chengzhc <314306545@qq.com>
2017-12-24 18:25:32 +0800

    part 2

--- app/src/main/java/top/chengzhen1971/weather/MainActivity.java
+++ app/src/main/java/top/chengzhen1971/weather/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.czstudio.czlibrary.CzLibrary;
@@ -20,8 +21,10 @@ public class MainActivity extends AppCompatActivity {
     //自身实例
     MainActivity instance;
 
-    Button btn_refresh;
-    TextView tv_info;
+    TextView tv_temp,tv_humidity,tv_pm25,tv_pm10,tv_quality,tv_sick,
+            tv_temp_low,tv_temp_hi,tv_sunrise,tv_sunset,tv_wind,tv_notice,tv_date;
+    ImageView iv_type;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,15 +43,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     void initView(){
+        tv_temp =findViewById(R.id.tv_temp);
+        tv_humidity =findViewById(R.id.tv_humidity);
+        tv_pm25 =findViewById(R.id.tv_pm25);
+        tv_pm10 =findViewById(R.id.tv_pm10);
+        tv_quality =findViewById(R.id.tv_quality);
+        tv_sick =findViewById(R.id.tv_sick);
+        tv_temp_low =findViewById(R.id.tv_temp_low);
+        tv_temp_hi =findViewById(R.id.tv_temp_hi);
+        tv_sunrise =findViewById(R.id.tv_sunrise);
+        tv_sunset =findViewById(R.id.tv_sunset);
+        tv_wind =findViewById(R.id.tv_wind);
+        tv_notice =findViewById(R.id.tv_notice);
+        tv_date = findViewById(R.id.tv_date);
 
-        tv_info = findViewById(R.id.tv_info);
-        btn_refresh = findViewById(R.id.btn_refresh);
-        btn_refresh.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getWeatherData();
-            }
-        });
+        iv_type=findViewById(R.id.iv_type);
     }
 
     void getWeatherData(){
@@ -101,13 +110,27 @@ public class MainActivity extends AppCompatActivity {
             JSONObject dataJson = weatherJson.getJSONObject("data");
             Log.e("Weather","dataJson="+dataJson.toString());
 
+            String temp = dataJson.getString("wendu");
+            String humidity = dataJson.getString("shidu");
+            String pm25 = dataJson.getString("pm25");
+            String pm10 = dataJson.getString("pm10");
+            String quality = dataJson.getString("quality");
+            String sick = dataJson.getString("ganmao");
+            tv_temp.setText(temp);
+            tv_humidity.setText("湿度："+humidity);
+            tv_pm25.setText("PM2.5："+pm25);
+            tv_pm10.setText("PM10："+pm10);
+            tv_quality.setText("空气质量："+quality);
+            tv_sick.setText("感冒指数："+sick);
+
+
             JSONArray forecastArray = dataJson.getJSONArray("forecast");
             Log.e("Weather","forecastArray="+forecastArray.toString());
 
             JSONObject todayJson = forecastArray.getJSONObject(0);
             Log.e("Weather","todayJson="+todayJson.toString());
 
-            String str_weather = "今天天气：\n";
+
             String date = todayJson.getString("date");
             String sunrise = todayJson.getString("sunrise");
             String high = todayJson.getString("high");
@@ -119,16 +142,14 @@ public class MainActivity extends AppCompatActivity {
             String type = todayJson.getString("type");
             String notice = todayJson.getString("notice");
 
-            str_weather = str_weather
-                    +"日期：" + date +"\n"
-                    + type +","+ fx +",风力"+ fl+"\n"
-                    +"气温：最"+ high +"，最"+ low +"\n"
-                    + "日出时间："+sunrise +"\n"
-                    + "日落时间："+sunset +"\n"
-                    + "出行提示："+ notice +"\n"
-                    + aqi +"\n";
+            tv_date.setText("日期："+date);
+            tv_temp_low.setText("最"+low);
+            tv_temp_hi.setText("最"+high);
+            tv_sunrise.setText("日出："+sunrise);
+            tv_sunset.setText("日落："+sunset);
+            tv_wind.setText("风向："+fx+",风力："+fl);
+            tv_notice.setText("出行建议：\n"+notice);
 
-            tv_info.setText(str_weather);
 
 
         }catch(Exception e){
